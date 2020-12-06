ece10950169f2911fc440951e7b74f04547581aa
Christian Mauduit (ufoot) <ufoot@ufoot.org>
2020-12-06 07:53:45 +0100

    [docker] 0.2.2, added symlinks to Godot executables

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
+docker build . -t ufoot/godot-rust-cross-compile:0.2.2
 docker build . -t ufoot/godot-rust-cross-compile:latest
-docker build . -t ufoot/godot-rust-cross-compile:0.2.1
---
+++ docker/push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker push ufoot/godot-rust-cross-compile:0.2.2
+docker push ufoot/godot-rust-cross-compile:latest
