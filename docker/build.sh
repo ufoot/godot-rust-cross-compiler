a9471623ee7e5fc299eb2785de114907bd11c6ea
Christian Mauduit (ufoot) <ufoot@ufoot.org>
2020-12-06 10:16:13 +0100

    [version] bump to 0.2.3

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t ufoot/godot-rust-cross-compiler:0.2.2
+docker build . -t ufoot/godot-rust-cross-compiler:0.2.3
 docker build . -t ufoot/godot-rust-cross-compiler:latest
--- docker/push.sh
+++ docker/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker push ufoot/godot-rust-cross-compiler:0.2.2
+docker push ufoot/godot-rust-cross-compiler:0.2.3
 docker push ufoot/godot-rust-cross-compiler:latest
